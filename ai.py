85e88c30ecd0e92caa440e77f1c69c3533f438be
byml-c <2014078563@qq.com>
2025-02-25 16:46:17 +0800

    Add bash variables feature.

--- ai.py
+++ ai.py
@@ -9,10 +9,7 @@ from pathlib import Path
 from openai import OpenAI
 
 # 导入 readline 模块，用于支持上下键选择历史记录
-if os.name == 'posix':
-    import readline
-elif os.name == 'nt':
-    import pyreadline3 as readline
+import readline
 
 # 配置文件路径
 ROOT_DIR = Path("/home/byml/projects/my-style/ai_agent")
@@ -46,7 +43,7 @@ class AIChat:
         with open(CONFIG_FILE, encoding="utf-8") as f:
             config = json.load(f)
             
-        if config["api_key"] == "your-api-key-here":
+        if config["api_key"] == "":
             print(f"请先配置API密钥: {CONFIG_FILE}")
             sys.exit(1)
         return config
@@ -63,7 +60,10 @@ class AIChat:
         if VARS_FILE.exists():
             with open(VARS_FILE, encoding="utf-8") as f:
                 return json.load(f)
-        return {}
+        return {
+            "users": {},
+            "bash": {}
+        }
     
     def save_config(self):
         """保存配置"""
@@ -143,12 +143,16 @@ class AIChat:
                 else:
                     # 开始回复
                     if delta.content != "" and is_answering == False:
-                        print("\n├─  󰛩  ANSWER", flush=True)
+                        if is_reasoning:
+                            print()
+                        print("├─  󰛩  ANSWER", flush=True)
                         is_answering = True
                     # 打印回复过程
                     print(delta.content, end="", flush=True)
                     answer_content += delta.content
-            print('\n╰─────────────')
+            if is_reasoning or is_answering:
+                print()
+            print('╰─────────────')
             self.history.append({
                 "role": "assistant",
                 "content": answer_content
@@ -159,6 +163,13 @@ class AIChat:
             print("╭─    中断")
             print("╰─  本轮对话已停止。")
     
+    def bash(self, cmd:str):
+        start_time = time.time()
+        result = subprocess.run(
+            cmd, shell=True, check=True, text=True, capture_output=True)
+        cost_time = int((time.time() - start_time) * 1000)
+        return result.stdout.strip(), cost_time, result.returncode
+
     def command(self, cmd:str):
         if cmd[:6] == 'change':
             if cmd == 'change':
@@ -182,7 +193,10 @@ class AIChat:
             else:
                 print(f"╰─    模型不存在")
             return 'done'
-        elif cmd == 'clear':
+        elif cmd in ['cls', 'clear']:
+            os.system('clear')
+            return 'done'
+        elif cmd == 'forget':
             self.history = [self.history[0]]
             print()
             print("╭─    清空历史")
@@ -196,48 +210,66 @@ class AIChat:
             return 'exit'
         elif cmd[:4] == 'bash':
             print()
-            print(f"╭─    Bash 命令")
+            print(f"╭─    终端命令")
             try:
-                start_time = time.time()
-                result = subprocess.run(
-                    cmd[5:], shell=True, check=True, text=True, capture_output=True)
-                cost_time = int((time.time() - start_time) * 1000)
-                print(result.stdout, end="")
-                print(f"╰─    执行结束，  {cost_time} ms, return {result.returncode}")
+                out, cost_time, returncode = self.bash(cmd[5:])
+                print(out, end="")
+                print(f"╰─    执行结束，  {cost_time} ms, return {returncode}")
             except KeyboardInterrupt:
-                print(f"╰─    中断执行，  {cost_time} ms, return {result.returncode}")
+                print(f"╰─    中断执行，  {cost_time} ms, return {returncode}")
             except Exception as e:
                 print(f"╰─    执行出错：{e}")
             return 'done'
-        elif cmd[:3] == 'var':
+        elif cmd[:4] == 'varr':
             print()
-            print(f"╭─    变量设置")
+            print(f"╭─    设置常值变量")
             try:
-                cmd = cmd[4:].split(' ', 1)
-                start_time = time.time()
-                result = subprocess.run(
-                    cmd[1:], shell=True, check=True, text=True, capture_output=True)
-                cost_time = int((time.time() - start_time) * 1000)
-                self.vars[cmd[0]] = result.stdout.strip()
+                cmd = cmd[5:].split(' ', 1)
+                out, cost_time, returncode = self.bash(cmd[1:])
+                self.vars["users"][cmd[0]] = out
                 self.save_vars()
-                print(f"├─  {cmd[0]} = \"{self.vars[cmd[0]]}\"")
-                print(f"╰─    设置成功，  {cost_time} ms, return {result.returncode}")
+                print(f"├─  {cmd[0]} = \"{self.vars['users'][cmd[0]]}\"")
+                print(f"╰─    设置成功，  {cost_time} ms, return {returncode}")
             except KeyboardInterrupt:
-                print(f"╰─    中断执行，  {cost_time} ms, return {result.returncode}")
+                print(f"╰─    中断执行，  {cost_time} ms, return {returncode}")
+            except Exception as e:
+                print(f"╰─    设置失败：{e}")
+            return 'done'
+        elif cmd[:4] == 'varc':
+            print()
+            print(f"╭─    设置终端变量")
+            try:
+                cmd = cmd[5:].split(' ', 1)
+                self.vars["bash"][cmd[0]] = cmd[:1]
+                self.save_vars()
+                print(f"├─  {cmd[0]} = \"{self.vars['bash'][cmd[0]]}\"")
+                print(f"╰─    设置成功")
+            except KeyboardInterrupt:
+                print(f"╰─    中断设置")
             except Exception as e:
                 print(f"╰─    设置失败：{e}")
             return 'done'
         elif cmd[:4] == 'show':
+            user_print, bash_print = False, False
+            def print_title(t:str):
+                nonlocal user_print, bash_print
+                if t == 'u' and not user_print:
+                    print("├─    常值文本变量")
+                    user_print = True
+                if t == 'b' and not bash_print:
+                    print("├─    终端命令变量")
+                    bash_print = True
             print()
             print(f"╭─    变量列表")
             cmd = cmd[5:].strip()
-            if cmd == '':
-                for k, v in self.vars.items():
-                    print(f"├─  {k} = \"{v}\"")
-            else:
-                for k, v in self.vars.items():
-                    if re.match(cmd, k):
-                        print(f"├─  {k} = \"{v}\"")
+            for k, v in self.vars["users"].items():
+                if cmd == '' or re.match(cmd, k):
+                    print_title('u')
+                    print(f"│   {k:6} = \"{v}\"")
+            for k, v in self.vars["bash"].items():
+                if cmd == '' or re.match(cmd, k):
+                    print_title('b')
+                    print(f"│   {k:6} = \"{self.bash(v)[0]}\"")
             print("╰─────────────")
             return 'done'
         else:
@@ -249,22 +281,32 @@ class AIChat:
                 print("╭─  󰘥  帮助")
             print("├─  命令语法为 /+命令，变量在对话时可通过 {var_name} 引用")
             print("├─    控制命令")
-            print("│   clear : 清空历史，变量不会清空")
+            print("│   cls   : 清空屏幕，历史、变量均不会清空")
+            print("│   forget: 清空历史，变量不会清空")
             print("│   change: 切换模型")
             print("│   help  : 查看帮助")
             print("│   exit  : 退出")
-            print("├─    Bash 命令")
-            print("│   bash <command>      : 结果将直接输出在对话框中")
-            print("│   var <name> <command>: 将结果保存在变量中")
-            print("│   show <option:name>  : 打印变量，支持正则，无参数时打印所有变量")
+            print("├─    终端命令")
+            print("│   bash <command>       : 结果将直接输出在对话框中")
+            print("│   varr <name> <command>: 将终端命令运行结果保存在变量中")
+            print("│   varc <name> <command>: 将终端命令直接保存在变量中")
+            print("│   show <option:name>   : 打印变量，支持正则，无参数时打印所有变量")
             print("╰─────────────")
             return 'done'
     
     def prase(self, ipt:str):
         """解析输入"""
+        def replace_var(match:re.Match):
+            if match is None: return ''
+            val = self.vars["users"].get(match.group(1))
+            if val is not None:
+                return val
+            val = self.vars["bash"].get(match.group(1))
+            if val is not None:
+                return self.bash(val)[0]
+            return match.group(0)
         # 替换变量
-        for k, v in self.vars.items():
-            ipt = ipt.replace(f"{{{k}}}", v)
+        ipt = re.sub(r"{(.*?)}", replace_var, ipt)
         return ipt
 
     def main(self):
