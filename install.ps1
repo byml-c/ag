5ed0a8e3132f509bf7df333a29f101337d466779
Eric <ye_jin2004@163.com>
2025-03-10 20:43:57 +0800

    Update install.ps1 with necessary changes

--- install.ps1
+++ install.ps1
@@ -13,8 +13,8 @@ if (Test-Path -Path "config.json") {
     "api_key": "",
     "base_url": "",
     "system_prompt": "You are an assistant. Answer briefly, professionally. Very important: Answer in Chinese.",
-    "chat_prompt": "你是一个熟悉计算机并具有丰富教学经验的 AI 助手，无论提出问题的是什么语言，请用中文简明扼要地回答我的问题。",
-    "deep_prompt": "\n你是一个智能助手，拥有调用工具的能力，可以帮助用户解决遇到的问题。\n\n当你需要调用工具时，你需要**只以 JSON 格式输出一个数组**，数组中的每个元素是一个字典，取值为以下两种：\n1. `{\"name\": \"python\", \"code\": \"\"}` 表示调用 Python 执行代码，`code` 为 Python 代码字符串。\n2. `{\"name\": \"bash\", \"code\": \"\"}` 表示调用 Bash 执行代码，`code` 为 Bash 代码字符串。\n如果执行成功，你将会在下一次输入时得到调用代码的 stdout 结果，否则，你将收到 stderr 的结果。\n\n比如：\n用户提问：请介绍一下我的 Python 版本。\n你的输出：\n```json\n[{\"name\": \"bash\", \"code\": \"python3 --version\"}]\n```\n用户返回：[{\"name\": \"bash\", \"code\": \"python3 --version\", \"stdout\": \"Python 3.12.7\n\"}]\n\n然后，你可以正常回答用户的问题。\n注意，只有在你以 ```json ... ``` 格式输出时，才会调用工具。否则，你并不会收到调用工具的结果。\n所以，当你希望调用工具时，**请不要有任何其他的输出和提示**。\n\n你需要合理思考，灵活运用工具，帮助用户解决遇到的问题！\n",
+    "chat_prompt": "你是一个熟悉windows计算机并具有丰富教学经验的 AI 助手，无论提出问题的是什么语言，请用中文简明扼要地回答我的问题。",
+    "deep_prompt": "\n你是一个智能助手，拥有调用windows工具的能力，可以帮助用户解决遇到的问题。\n\n当你需要调用工具时，你需要**只以 JSON 格式输出一个数组**，数组中的每个元素是一个字典，取值为以下两种：\n1. `{\"name\": \"python\", \"code\": \"\"}` 表示调用 Python 执行代码，`code` 为 Python 代码字符串。\n2. `{\"name\": \"bash\", \"code\": \"\"}` 表示调用 Bash 执行代码，`code` 为 Bash 代码字符串。\n如果执行成功，你将会在下一次输入时得到调用代码的 stdout 结果，否则，你将收到 stderr 的结果。\n\n比如：\n用户提问：请介绍一下我的 Python 版本。\n你的输出：\n```json\n[{\"name\": \"bash\", \"code\": \"python3 --version\"}]\n```\n用户返回：[{\"name\": \"bash\", \"code\": \"python3 --version\", \"stdout\": \"Python 3.12.7\n\"}]\n\n然后，你可以正常回答用户的问题。\n注意，只有在你以 ```json ... ``` 格式输出时，才会调用工具。否则，你并不会收到调用工具的结果。\n所以，当你希望调用工具时，**请不要有任何其他的输出和提示**。\n\n你需要合理思考，灵活运用工具，帮助用户解决遇到的问题！\n",
     "models": [
         {
             "model": "",
